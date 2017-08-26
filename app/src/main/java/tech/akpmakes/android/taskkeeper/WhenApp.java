098a529c8cfd3ff200ce2eec325bd8a5f00a5f18
Austin Peterson <austin@akpwebdesign.com>
2017-08-25 21:55:50 -0400

    fix: don't run at all in Test Lab
    Fixes #36

--- app/src/main/java/tech/akpmakes/android/taskkeeper/WhenApp.java
+++ app/src/main/java/tech/akpmakes/android/taskkeeper/WhenApp.java
@@ -1,6 +1,7 @@
 package tech.akpmakes.android.taskkeeper;
 
 import android.app.Application;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatDelegate;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -9,6 +10,13 @@ public class WhenApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        String testLabSetting = Settings.System.getString(getContentResolver(), "firebase.test.lab");
+        if ("true".equals(testLabSetting)) {
+            // Kill the process if we're running in Test Lab. We don't like Test Lab at the moment.
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+
         /* Enable disk persistence  */
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
